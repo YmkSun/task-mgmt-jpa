e50c8207d749f95ea6367e2bdb7a896a095a05da
ymksun <ymksun90@gmail.com>
2019-02-19 14:47:37 +0630

    HomeController Implementation

---
+++ src/main/java/com/ymksun/taskmgmt/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ymksun.taskmgmt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+}
