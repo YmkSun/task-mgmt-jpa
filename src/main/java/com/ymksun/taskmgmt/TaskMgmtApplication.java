a859430abd664187628f554196e1403d09f4c828
ymksun <ymksun90@gmail.com>
2019-03-11 17:56:13 +0630

    CORS configuration...

--- src/main/java/com/ymksun/taskmgmt/TaskMgmtApplication.java
+++ src/main/java/com/ymksun/taskmgmt/TaskMgmtApplication.java
@@ -4,7 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
 public class TaskMgmtApplication {
 
@@ -13,6 +18,17 @@ public class TaskMgmtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskMgmtApplication.class, args);
 		logger.info("Applaction Started...");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST",
+						"PUT", "DELETE");
+			}
+		};
 	}
 
 }
--- src/main/java/com/ymksun/taskmgmt/controller/LoginController.java
+++ src/main/java/com/ymksun/taskmgmt/controller/LoginController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ymksun.taskmgmt.model.dto.UserDto;
@@ -18,9 +18,9 @@ public class LoginController {
 	@Autowired
 	UserService userService;
 
-	@GetMapping("/")
-	public ResponseEntity<UserDto> login(@PathVariable(value = "id") String userId,
-			@PathVariable(value = "pwd") String password) {
+	@GetMapping("")
+	public ResponseEntity<UserDto> login(@RequestParam(value = "id") String userId,
+			@RequestParam(value = "pwd") String password) {
 		return new ResponseEntity<>(userService.login(userId, password), HttpStatus.OK);
 	}
 
